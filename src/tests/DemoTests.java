76a620f3fe5b0a315d73e8faad748503acd27f08
JelenaSabo <jelenasabo789@gmail.com>
2021-02-04 21:04:11 +0100

    kreiranje projekta

---
+++ src/tests/DemoTests.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DemoTests {
+	@Test
+	public void demoTest() {
+		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+	}
+}
